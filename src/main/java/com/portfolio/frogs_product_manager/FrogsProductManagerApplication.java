451b0d7f78f92095a15d8a1c5e72bbc57e5bcc35
Lucas Saporetti <lucassaporetti@gmail.com>
2024-06-11 13:39:34 -0300

    first commit

---
+++ src/main/java/com/portfolio/frogs_product_manager/FrogsProductManagerApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.frogs_product_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogsProductManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogsProductManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/frogs_product_manager/FrogsProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.frogs_product_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogsProductManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
